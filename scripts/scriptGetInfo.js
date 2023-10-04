c82dd6acf3003f648cb0a8180e616f52486ec49a
trpl-A <amr.swartland@gmail.com>
2023-10-04 15:28:37 +0200

    updated

---
+++ scripts/scriptGetInfo.js
@@ -0,0 +1,11 @@
+
+function getInfo() {
+    let name = document.getElementById("name").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone-num").value
+
+    // display 
+    console.log(name, email, phone)
+
+    window.alert("pause")
+}
